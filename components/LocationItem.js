de087f3708e7687fc2072d98818fc04d39853f6c
malkio <coding.malkio@gmail.com>
2020-08-06 16:15:54 +0800

    Remove icon

--- components/LocationItem.js
+++ components/LocationItem.js
@@ -3,6 +3,8 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { AnimatedFadeImage } from './index.js';
 import Feather from 'react-native-vector-icons/Feather';
 
+// {isPurchased ? <View style={{position:'absolute', top:0,right:0, paddingVertical:4, paddingHorizontal:6, backgroundColor:'#E6BF80', zIndex:99}}><Text style={{color:'#fff', fontSize:14}}>Purchased</Text></View> : null}
+// {!isPurchased ? <View style={{position:'absolute', top:160,right:0, padding:4, }}><Feather name={"shopping-cart"} size={28} color={"#000"} /></View> : null}
 const LocationItem = ({image,title,isPurchased,venuesCount, onPress, width}) =>{
 	return (
 		<TouchableOpacity activeOpacity={0.8} onPress={onPress} style={{marginBottom:14}}>
@@ -10,8 +12,7 @@ const LocationItem = ({image,title,isPurchased,venuesCount, onPress, width}) =>{
 				<AnimatedFadeImage source={{uri:image}} resizeMode={"cover"} style={{width:width, height:150}} containerStyle={{marginBottom:4, backgroundColor:'ghostwhite'}}/>
 	      <Text style={{fontSize:16, marginTop:4, marginBottom:2}}>{title}</Text>
 				<Text style={{fontSize:14, color:'gray'}}>{venuesCount+" Venues"}</Text>
-				{isPurchased ? <View style={{position:'absolute', top:0,right:0, paddingVertical:4, paddingHorizontal:6, backgroundColor:'#E6BF80', zIndex:99}}><Text style={{color:'#fff', fontSize:14}}>Purchased</Text></View> : null}
-				{!isPurchased ? <View style={{position:'absolute', top:160,right:0, padding:4, }}><Feather name={"shopping-cart"} size={28} color={"#000"} /></View> : null}
+
 	    </View>
 		</TouchableOpacity>);
 }
